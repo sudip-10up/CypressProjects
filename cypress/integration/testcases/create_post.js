103a8af3e45d5dc33dd5b99f29f2ceb388a755e6
sudip-10up <94599521+sudip-10up@users.noreply.github.com>
2022-02-07 23:29:41 +0530

    Setup
    commit

---
+++ cypress/integration/testcases/create_post.js
@@ -0,0 +1,38 @@
+/// <reference types="cypress" />
+describe('Create Post',()=> {
+
+    beforeEach(()=>{
+
+        cy.admin_login('admin','admin')
+        
+    })
+    it('Create New Post', ()=> {
+
+        
+        cy.get('#menu-posts > .wp-has-submenu > .wp-menu-name').click()
+        cy.get('.page-title-action').click()
+        cy.get('body').trigger('keydown', { keyCode: 27})
+        cy.get('.edit-post-visual-editor__post-title-wrapper > .wp-block').type('Test Post')
+        
+        cy.get('.block-editor-default-block-appender > .components-dropdown > .components-button > svg').click()
+        cy.get('#components-search-control-0').type('Heading')
+        cy.contains('Heading').click()
+        cy.get('.block-editor-rich-text__editable.block-editor-block-list__block.wp-block.is-selected.wp-block-heading.rich-text').type('Test Heading')
+        
+
+        cy.get('.block-editor-default-block-appender > .components-dropdown > .components-button > svg').click()
+        cy.get('.components-search-control__input').type('List')
+        cy.contains('List').click()
+        cy.get('.block-editor-rich-text__editable.block-editor-block-list__block.wp-block.is-selected.wp-block-list.rich-text').type('123{enter}')
+        cy.get('.block-editor-rich-text__editable.block-editor-block-list__block.wp-block.is-selected.wp-block-list.rich-text').type('456')
+        
+
+
+
+
+
+        cy.get('.editor-post-publish-panel__toggle').click()
+        cy.get('.editor-post-publish-panel__header-publish-button > .components-button').click()
+
+    })
+})
---
+++ cypress/integration/testcases/plugin_installation.js
@@ -0,0 +1,36 @@
+/// <reference types="cypress" />
+describe('OSS Pluding Test',()=>{
+
+    beforeEach(()=>{
+
+        cy.admin_login('admin','admin')
+        
+    })
+    it('Plugin Upload and Activation', ()=> {
+        
+        cy.get('#menu-plugins > .wp-has-submenu > .wp-menu-name').click()
+        cy.get('.page-title-action').click()
+        cy.get('.upload-view-toggle').click()
+    
+        const filepath = 'restricted-site-access-develop.zip'
+        cy.get('input[type="file"]').attachFile(filepath)
+        cy.get('#install-plugin-submit').click()
+        cy.visit('http://oss.local/wp-admin/plugins.php')
+        cy.get('#activate-restricted-site-access').click()
+        cy.get("<p>Plugin activated.</p>",{timeout:3000}).should(($x)=>{
+
+            expect($x).to.have.toString('Plugin activated');
+        })    
+    })
+    it('Deactivate Plugin', ()=> {
+
+        cy.get('#menu-plugins > .wp-has-submenu > .wp-menu-name').click()
+        cy.get('#deactivate-restricted-site-access').click()
+    })
+    it('Delete Plugin', ()=> {
+
+       cy.get('#menu-plugins > .wp-has-submenu > .wp-menu-name').click()
+        cy.get('#delete-restricted-site-access').click()
+   });
+
+    })
